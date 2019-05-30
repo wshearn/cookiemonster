e2aed1eba2572a917af6db0bf19fbc45513d3ee9
Wesley Hearn <wesley@shearn.me>
2019-05-30 16:34:39 -0400

    Add path to the cookie

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/api/hey.jpg', function(req, res, next) {
   console.log(req.cookies);
   console.log(req.signedCookies);
   var img = fs.readFileSync('./images/1234.jpg');
-  res.cookie("openshift-session-token", "derp", { domain: ".devshift.org", path: "/api/", httpOnly: true }).send('cookie set');
+  res.cookie("openshift-session-token", "derp", { domain: ".devshift.org", path: "/api/", httpOnly: true });
   res.writeHead(200, {'Content-Type': 'image/jpg'});
   res.end(img, 'binary');  
 });
